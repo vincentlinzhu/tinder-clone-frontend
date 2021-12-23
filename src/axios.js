1b6540fb0b145cc9852c3a10ba61b7e7ce3a0e53
Vincent Zhu <vincentzhu@ucs.edu>
2021-12-23 03:55:03 -0800

    Yaaa

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://clone--tinder--full.herokuapp.com/",
+  baseURL: "https://clone--tinder--backend.herokuapp.com/",
 });
 
 export default instance;
